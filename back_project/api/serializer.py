7373c7edf6169e1bf51baf8b48a0e58fa1761572
teozz17 <m.barbachan17@gmail.com>
2025-01-04 21:56:30 -0300

    Add: Serializer and Urls

---
+++ back_project/api/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Airport
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ['name', 'link', 'icao', 'description', 'visited']
---
+++ back_project/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .airport_view import get_airports, create_airport, update_airport, delete_airport, get_airport_by_icao
+from .user_view import create_user, login_user
+
+urlpatterns = [
+    path('register/', create_user, name='create_user'),
+    path('login/', login_user, name='login_user'),
+    path('airports/', get_airports, name='get_airports'),
+    path('airports/create/', create_airport, name='create_airport'),
+    path('airports/<int:airport_id>/', update_airport, name='update_airport'),
+    path('airports/<int:airport_id>/', delete_airport, name='delete_airport'),
+    path('airports/<str:icao>/', get_airport_by_icao, name='get_airport_by_icao')
+]
+
